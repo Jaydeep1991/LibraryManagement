d63c667893d40a9dfd0056a856ef44f0581993b5
jaydeep1991 <jaydeeptomal@hotmail.com>
2020-05-10 10:54:53 +0530

    release 10-05-2020

--- src/app/BookService/book.service.ts
+++ src/app/BookService/book.service.ts
@@ -5,6 +5,12 @@ import { environment } from 'src/environments/environment';
 export class getBookId{
   constructor(public statusCode:number,public registrationNumber:number,public message:string){}
 }
+export class data{
+  constructor(public id:number,public bookgener:string){}
+}
+export class bookType{
+  constructor(public statusCode:number,public message:string,public data:Array<data>){}
+}
 
 @Injectable({
   providedIn: 'root'
@@ -16,6 +22,10 @@ export class BookService {
    
     getBookRegistrationNumber(){
       return this.http.get<getBookId>(`${environment.url}/api/books/getBookId`);
+    }
+
+    getListOfBookType(){
+      return this.http.get<bookType>(`${environment.url}/api/books/showAllBookType`);
     }
   
   
--- src/app/addbooks/addbooks.component.ts
+++ src/app/addbooks/addbooks.component.ts
@@ -2,6 +2,10 @@ import { Component, OnInit } from '@angular/core';
 import { BookService } from '../BookService/book.service';
 import { ToasterService } from '../toasterService/toaster.service';
 
+export class Books{
+  constructor(public id:number,public bookgener:string){}
+}
+
 @Component({
   selector: 'app-addbooks',
   templateUrl: './addbooks.component.html',
@@ -14,11 +18,12 @@ export class AddbooksComponent implements OnInit {
   bookName:string='';
   authorName:string='';
   bookPrice:number;
-  bookType:string='';
+  bookType:Books;
   
   constructor(private bookService: BookService,private toasterService: ToasterService) { }
 
   ngOnInit(): void {
+    this.getlistBook();
   }
 
   generateId() {
@@ -46,6 +51,20 @@ export class AddbooksComponent implements OnInit {
   fatal(err) {
     this.errorMessage = err.mesage;
     this.toasterService.Error('Something went Wrong');
+  }
+
+  getlistBook(){
+    this.bookService.getListOfBookType().subscribe(
+      resp=>{
+        if(resp.statusCode===200){
+          this.successfullBookList(resp);          
+        }
+      }
+    )
+  }
+  successfullBookList(resp){
+    this.bookType=resp.data;
+    console.log('Book type is ',this.bookType);
   }
 
 }
